f7c7fde765efd6c9d80ef5268486de5967cec50a
Kenan Kurda <intecbrussel24@gmail.com>
2024-05-07 14:50:17 +0200

    Add project files.

---
+++ ViewModelDemo/Models/ViewModel/ProductSupplierViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewModelDemo.Models.ViewModel
+{
+    public class ProductSupplierViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Supplier> Suppliers { get; set; }
+    }
+}
